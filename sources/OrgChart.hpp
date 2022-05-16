5a67dbbc16e0c3f84b589f48828d999d925f73d8
LeeadJ <leead123@gmail.com>
2022-05-16 12:29:00 +0300

    implemented OrgChart.hpp

---
+++ sources/OrgChart.hpp
@@ -0,0 +1,57 @@
+#pragma once
+#include<iostream>
+#include<vector>
+#include<string>
+#include "Node.hpp"
+
+namespace ariel{
+    class OrgChart{
+        std::vector<Node*> _chartNodes;
+
+        public:
+            //Getters:
+            std::vector<Node*> getChartNodes() {return this->_chartNodes;}
+
+            //Constructors:
+            OrgChart(); //initialization with no arguments
+
+            //Destructor:
+            ~OrgChart();
+
+            //Functions:
+            OrgChart& addRoot(const std::string& root);
+            OrgChart& add_sub(const std::string& parent, const std::string& child);
+            friend std::ostream& operator << (std::ostream& out, const OrgChart& chart);
+
+            //Iterator
+            class iterator{
+                int _size;
+
+                public:
+                    //Constructor
+                    iterator();
+
+                    //Functions:
+                    int size() const {return this->_size;}
+                    bool operator != (const iterator& other) const;
+                    bool operator == (const iterator& other) const;
+                    std::string* operator -> (); //returns a pointer
+                    std::string* operator * (); //returns the value of the string
+                    iterator& operator ++ (); 
+                    iterator& operator ++ (int);
+            };
+            
+            //Iterator Functions:
+            iterator begin_level_order() const;
+            iterator end_level_order() const;
+
+            iterator begin_reverse_order() const;
+            iterator end_reverse_order() const;
+
+            iterator begin_preorder() const;
+            iterator end_preorder() const;
+
+            friend std::ostream& operator << (const std::ostream& out, const iterator& itr);
+
+    };
+}
